2aca01213e5990a905d3c824b895fd1b3a1cf7fd
badri8 soldaat <113472@emmauscollege.nl>
2025-02-07 10:43:57 +0000

    changes in db

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,11 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Rock Solid', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
-
+insert into products (name, description, code, price) values ('Vespa', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 1200);
+insert into products (name, description, code, price) values ('Piaggio', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 2000);
+insert into products (name, description, code, price) values ('NIU', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 1350);
+insert into products (name, description, code, price) values ('Peugeot', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
+insert into products (name, description, code, price) values ('Horwin', '', '686928463-6', 14);
+insert into products (name, description, code, price) values ('SYM', '', '', 14);
+insert into products (name, description, code, price) values ('Kymco', '', '', 16);
+insert into products (name, description, code, price) values ('AGM', '', '', 16);
