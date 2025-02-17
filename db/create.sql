f05d1586c27dd6f25b8df8bd790235fce4fe3009
badri8 soldaat <113472@emmauscollege.nl>
2025-02-17 11:31:27 +0000

    plaatjes

--- db/create.sql
+++ db/create.sql
@@ -20,11 +20,11 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Sprint', 'Merk: Vespa', '816905633-0', 1200);
-insert into products (name, description, code, price) values ('Primavera', 'Merk: Vespa', '077030122-3', 2000);
-insert into products (name, description, code, price) values ('GTS Super', 'Merk: Vespa', '445924201-X', 1350);
-insert into products (name, description, code, price) values ('Elettrica', 'Merk: Vespa', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Agility S', 'Merk: Kymco', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Filly 50', 'Merk: Kymco', '', 14);
-insert into products (name, description, code, price) values ('Sky Town 50', 'Merk: Kymco', '', 16);
-insert into products (name, description, code, price) values ('New Like Euro5', 'Merk: Kymco', '', 16);
+insert into products (name, description, code, price) values ('Sprint', 'Merk: Vespa', '816905633-0', 4999);
+insert into products (name, description, code, price) values ('Primavera', 'Merk: Vespa', '077030122-3', 3899);
+insert into products (name, description, code, price) values ('GTS', 'Merk: Vespa', '445924201-X', 5499);
+insert into products (name, description, code, price) values ('946 Snake (Limited Edition)', 'Merk: Vespa', '693155505-7', 11999);
+insert into products (name, description, code, price) values ('Agility FR', 'Merk: Kymco', '686928463-6',1999);
+insert into products (name, description, code, price) values ('Filly 50', 'Merk: Kymco', '', 1599);
+insert into products (name, description, code, price) values ('Sky Town 50', 'Merk: Kymco', '', 2499);
+insert into products (name, description, code, price) values ('New Like Euro5', 'Merk: Kymco', '', 2999);
